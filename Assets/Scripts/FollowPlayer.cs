82677e726ad273a89665721f4f51e9d2e7f14f88
jtpi <jtrinopi@gmail.com>
2023-04-08 21:55:07 +0100

    End of part 1.3

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{ 
-    public float speed = 10;
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -10);
 
     // Start is called before the first frame update
     void Start()
     {
         
     }
-    
+
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        //Vamos mover o carro para a frente
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.position = player.transform.position + offset;
     }
 }
